1220f750109c4bdae5fc67d70b18cbc3464ad818
rubenmafe <ruben.marquezf@e-campus.uab.cat>
2019-05-17 21:43:45 +0200

    App implemented

--- app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
+++ app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,15 +14,47 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-	private TextView txvResult;
+	//Layout
+	private ImageView image11;
+	private ImageView image12;
+	private ImageView image13;
+	private ImageView image21;
+	private ImageView image22;
+	private ImageView image23;
+	private ImageView image31;
+	private ImageView image32;
+	private ImageView image33;
+	private ImageView restart;
+	private TextView title;
+
+	//Atributes
+	private boolean user;
+	private ArrayList<ArrayList<String>> listOLists;
+	private String userNumber;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		txvResult = (TextView) findViewById(R.id.txvResult);
+		image11 = (ImageView) findViewById(R.id.img11);
+		image12 = (ImageView) findViewById(R.id.img12);
+		image13 = (ImageView) findViewById(R.id.img13);
+		image21 = (ImageView) findViewById(R.id.img21);
+		image22 = (ImageView) findViewById(R.id.img22);
+		image23 = (ImageView) findViewById(R.id.img23);
+		image31 = (ImageView) findViewById(R.id.img31);
+		image32 = (ImageView) findViewById(R.id.img32);
+		image33 = (ImageView) findViewById(R.id.img33);
+		restart = (ImageView) findViewById(R.id.restart);
+
+		title = (TextView) findViewById(R.id.title);
+
+		this.user = true;
+
+		restartArray();
 	}
 
+	// Actions
 	public void getSpeechInput(View view) {
 
 		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -35,6 +68,12 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
+	public void restart(View view) {
+		this.restart.setVisibility(View.INVISIBLE);
+		this.title.setVisibility(View.INVISIBLE);
+		restartArray();
+	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
@@ -43,9 +82,240 @@ public class MainActivity extends AppCompatActivity {
 			case 10:
 				if (resultCode == RESULT_OK && data != null) {
 					ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-					txvResult.setText(result.get(0));
+					String stringResult = result.get(0);
+
+					ImageView imageView = null;
+					String newValue = this.user ? "1" : "2";
+					String currentValue = "";
+					switch (stringResult) {
+						case "11":
+							currentValue = this.listOLists.get(0).get(0);
+							if (currentValue.equals("0")) {
+								imageView = this.image11;
+								this.listOLists.get(0).set(0, newValue);
+							}
+							break;
+						case "12":
+							currentValue = this.listOLists.get(0).get(1);
+							if (currentValue.equals("0")) {
+								imageView = this.image12;
+								this.listOLists.get(0).set(1, newValue);
+							}
+							break;
+						case "13":
+							currentValue = this.listOLists.get(0).get(2);
+							if (currentValue.equals("0")) {
+								imageView = this.image13;
+								this.listOLists.get(0).set(2, newValue);
+							}
+							break;
+						case "21":
+							currentValue = this.listOLists.get(1).get(0);
+							if (currentValue.equals("0")) {
+								imageView = this.image21;
+								this.listOLists.get(1).set(0, newValue);
+							}
+							break;
+						case "22":
+							currentValue = this.listOLists.get(1).get(1);
+							if (currentValue.equals("0")) {
+								imageView = this.image22;
+								this.listOLists.get(1).set(1, newValue);
+							}
+							break;
+						case "23":
+							currentValue = this.listOLists.get(1).get(2);
+							if (currentValue.equals("0")) {
+								imageView = this.image23;
+								this.listOLists.get(1).set(2, newValue);
+							}
+							break;
+						case "31":
+							currentValue = this.listOLists.get(2).get(0);
+							if (currentValue.equals("0")) {
+								imageView = this.image31;
+								this.listOLists.get(2).set(0, newValue);
+							}
+							break;
+						case "32":
+							currentValue = this.listOLists.get(2).get(1);
+							if (currentValue.equals("0")) {
+								imageView = this.image32;
+								this.listOLists.get(2).set(1, newValue);
+							}
+							break;
+						case "33":
+							currentValue = this.listOLists.get(2).get(2);
+							if (currentValue.equals("0")) {
+								imageView = this.image33;
+								this.listOLists.get(2).set(2, newValue);
+							}
+							break;
+						default:
+							break;
+					}
+
+					if (imageView != null)  {
+						imageView.setVisibility(View.VISIBLE);
+						if(this.user) {
+							imageView.setImageResource(R.mipmap.kim);
+							this.user = !this.user;
+						} else {
+							imageView.setImageResource(R.mipmap.donald);
+							this.user = !this.user;
+						}
+						this.title.setVisibility(View.INVISIBLE);
+					} else {
+						this.title.setText("Invalid, try again");
+						this.title.setVisibility(View.VISIBLE);
+					}
+
+					if (isWinner()) {
+						this.title.setText(String.format("Player %s wins", this.userNumber));
+						this.title.setVisibility(View.VISIBLE);
+						setFinish();
+					}
 				}
 				break;
 		}
 	}
+
+	private boolean isWinner() {
+		boolean isWinner;
+
+		//Check the rows
+		String first = this.listOLists.get(0).get(0);
+		String second = this.listOLists.get(0).get(1);
+		String third = this.listOLists.get(0).get(2);
+
+		isWinner = this.threeStringsAreEqual(first, second, third);
+
+		if (isWinner) {
+			this.userNumber = first;
+			return isWinner;
+		}
+
+		first = this.listOLists.get(1).get(0);
+		second = this.listOLists.get(1).get(1);
+		third = this.listOLists.get(1).get(2);
+
+		isWinner = this.threeStringsAreEqual(first, second, third);
+
+		if (isWinner) {
+			this.userNumber = first;
+			return isWinner;
+		}
+
+		first = this.listOLists.get(2).get(0);
+		second = this.listOLists.get(2).get(1);
+		third = this.listOLists.get(2).get(2);
+
+		isWinner = this.threeStringsAreEqual(first, second, third);
+
+		if (isWinner) {
+			this.userNumber = first;
+			return isWinner;
+		}
+
+		//Check the columns
+		first = this.listOLists.get(0).get(0);
+		second = this.listOLists.get(1).get(0);
+		third = this.listOLists.get(2).get(0);
+
+		isWinner = this.threeStringsAreEqual(first, second, third);
+
+		if (isWinner) {
+			this.userNumber = first;
+			return isWinner;
+		}
+
+		first = this.listOLists.get(0).get(1);
+		second = this.listOLists.get(1).get(1);
+		third = this.listOLists.get(2).get(1);
+
+		isWinner = this.threeStringsAreEqual(first, second, third);
+
+		if (isWinner) {
+			this.userNumber = first;
+			return isWinner;
+		}
+
+		first = this.listOLists.get(0).get(2);
+		second = this.listOLists.get(1).get(2);
+		third = this.listOLists.get(2).get(2);
+
+		isWinner = this.threeStringsAreEqual(first, second, third);
+
+		if (isWinner) {
+			this.userNumber = first;
+			return isWinner;
+		}
+
+		//Check the diagonal
+		first = this.listOLists.get(0).get(0);
+		second = this.listOLists.get(1).get(1);
+		third = this.listOLists.get(2).get(2);
+
+		isWinner = this.threeStringsAreEqual(first, second, third);
+
+		if (isWinner) {
+			this.userNumber = first;
+			return isWinner;
+		}
+
+		first = this.listOLists.get(0).get(2);
+		second = this.listOLists.get(1).get(1);
+		third = this.listOLists.get(2).get(0);
+
+		isWinner = this.threeStringsAreEqual(first, second, third);
+
+		if (isWinner) {
+			this.userNumber = first;
+			return isWinner;
+		}
+
+		return isWinner;
+	}
+
+	private boolean threeStringsAreEqual(String first, String second, String third) {
+		boolean areEquals;
+		areEquals = (first.equals(second));
+		return (second.equals(third) && areEquals && first != "0");
+
+	}
+
+	private void setFinish() {
+		this.image11.setVisibility(View.INVISIBLE);
+		this.image12.setVisibility(View.INVISIBLE);
+		this.image13.setVisibility(View.INVISIBLE);
+		this.image21.setVisibility(View.INVISIBLE);
+		this.image22.setVisibility(View.INVISIBLE);
+		this.image23.setVisibility(View.INVISIBLE);
+		this.image31.setVisibility(View.INVISIBLE);
+		this.image32.setVisibility(View.INVISIBLE);
+		this.image33.setVisibility(View.INVISIBLE);
+		this.restart.setVisibility(View.VISIBLE);
+	}
+
+	private void restartArray() {
+        this.listOLists = new ArrayList<>();
+
+        ArrayList<String> linea1 = new ArrayList<>();
+        linea1.add("0");
+        linea1.add("0");
+        linea1.add("0");
+        ArrayList<String> linea2 = new ArrayList<>();
+        linea2.add("0");
+        linea2.add("0");
+        linea2.add("0");
+        ArrayList<String> linea3 = new ArrayList<>();
+        linea3.add("0");
+        linea3.add("0");
+        linea3.add("0");
+
+        this.listOLists.add(linea1);
+        this.listOLists.add(linea2);
+        this.listOLists.add(linea3);
+    }
+
 }
